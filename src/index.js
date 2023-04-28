f2764a0d7e811232181d03a123d8895f0e5000fa
Geane Ramos <geaneramos1995@gmail.com>
2023-04-28 16:03:21 -0300

    ajuste de indentações e alterações nos arquivos de js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-const btn = document.getElementById("btn");
-btn.addEventListener('click',start);
+const btn = document.getElementById("btn"); /* Aqui estamos puxando o id do botão do index.html */
+btn.addEventListener("click", start); /* Aqui estamos querendo chamando o evento de clicar em um botão */
 
-function start(){
-  const numC = document.getElementById('ccNumber').value;
-  const alert = document.getElementById ('result');
+function start() {
+  const numC = document.getElementById("ccNumber").value;
+  const alert = document.getElementById("result");
 
   const res = validator.isValid(numC);
   const mask = validator.maskify(numC);
-    
-  if (numC === ""){
+
+  console.log(mask)
+  if (numC === "") {
     alert.innerHTML = "Insira um valor";
+  } else if (res === true) {
+    alert.innerText = "Cartão Válido!";
+  } else {
+    alert.innerText = "Cartão inválido.";
   }
-
-  else if(res === true){
-    alert.innerText = "Cartão Válido!"
-  }
-
-  else{
-    alert.innerText = "Cartão inválido."
-  }
-
 }
-
 
