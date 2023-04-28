08816b62fde93088e4c9e2a213fc3da2aadd084b
Geane Ramos <geaneramos1995@gmail.com>
2023-04-28 17:40:44 -0300

    ajuste no arquivo index.html

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import validator from "./validator.js";
 const btn = document.getElementById("btn"); /* Aqui estamos puxando o id do botão do index.html */
 btn.addEventListener("click", start); /* Aqui estamos querendo chamando o evento de clicar em um botão */
 
-function start() {
-  const numC = document.getElementById("ccNumber").value;
-  const alert = document.getElementById("result");
+function start() /* Estamos definindo a função mencionada acima. */{
+  const numC = document.getElementById("ccNumber").value; /* Aqui estamos puxando o id do input do usuário no index.html. */
+  const alert = document.getElementById("result"); /* Aqui estamos puxando o parágrafo abaixo do botão, onde sairá o resultado. */
 
-  const res = validator.isValid(numC);
-  const mask = validator.maskify(numC);
+  const res = validator.isValid(numC); /* Aqui estamos puxando a função isValid, impostada do validator.js. */
+  const mask = validator.maskify(numC); /* Aqui estamos puxando a função maskify, impostada do validator.js. */
 
   console.log(mask)
   if (numC === "") {
-    alert.innerHTML = "Insira um valor";
+    alert.innerHTML = "Insira o número do cartão"; /* mensagem que aparecerá na tela caso o usuário clicar no botão sem inserir o número do cartão. */
   } else if (res === true) {
-    alert.innerText = "Cartão Válido!";
+    alert.innerText = "Cartão Válido!"; /* mensagem que aparecerá na tela caso o usuário inserir um cartão válido e clicar. */
   } else {
-    alert.innerText = "Cartão inválido.";
+    alert.innerText = "Cartão inválido."; /* mensagem que aparecerá na tela caso o usuário inserir um cartão inválido e clicar. */
   }
 }
 
