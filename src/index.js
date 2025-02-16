79a4d946e1c207dca8ba508cf187d5f436726e83
Geane Ramos <geaneramos1995@gmail.com>
2025-02-15 21:37:58 -0300

    refactor: remove comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import validator from "./validator.js";
 
-const btn = document.getElementById("btn"); /* Aqui estamos puxando o id do botão do index.html. */
-btn.addEventListener("click", start); /* Aqui estamos chamando o evento de clicar em um botão. */
+const btn = document.getElementById("btn");
+btn.addEventListener("click", start);
 
-function start(event) { /* Estamos definindo a função mencionada acima (start). */
-  event.preventDefault() /* O event.preventDefault() impede que a página atualize a cada clique. */
-  const numC = document.getElementById("ccNumber").value; /* Aqui estamos puxando o id do input do usuário no index.html. */
-  const alert = document.getElementById("result"); /* Aqui estamos puxando o parágrafo abaixo do botão, onde sairá o resultado. */
+function start(event) {
+  event.preventDefault();
+  const numC = document.getElementById("ccNumber").value;
+  const alert = document.getElementById("result");
 
-  const res = validator.isValid(numC); /* Aqui estamos puxando a função isValid, impostada do validator.js. */
-  const mask = validator.maskify(numC); /* Aqui estamos puxando a função maskify, impostada do validator.js. */
+  const res = validator.isValid(numC);
+  const mask = validator.maskify(numC);
 
   if (numC === "") {
-    alert.innerHTML = "Insira o número do cartão"; /* Mensagem que aparecerá na tela caso o usuário clicar no botão sem inserir o número do cartão. */
+    alert.innerHTML = "Insira o número do cartão";
   } else if (res === true) {
-    alert.innerText = `O número do cartão ${mask} é Válido!`; /* Template strings são envolvidas por (` `) em vez de aspas simples ou duplas. Template strings podem possuir placeholders. Estes são indicados por um cifrão seguido de chaves (${expression}). As expressões nos placeholders, bem como o texto em volta delas são passados a uma função. A função padrão apenas concatena as partes em uma string única.*/ 
+    alert.innerText = `O número do cartão ${mask} é Válido!`;
   } else {
-    alert.innerText = "Cartão inválido."; /* Mensagem que aparecerá na tela caso o usuário inserir um cartão inválido e clicar. */
+    alert.innerText = "Cartão inválido.";
   }
 }
-
